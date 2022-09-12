c45df0e4d067482c475afa03dfbfcdb11a5e5441
Carlos Almeyda <carlitos990321@gmail.com>
2022-09-12 09:53:13 -0300

    ..

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,12 +6,14 @@ let color = document.querySelector("#color");
 let botonCalcular = document.querySelector("#boton");
 
 let nombreUsuario = document.querySelector("#nombre-usuario")
+let cantidadFinal =document.querySelector("#cantidad-final")
 let colorFinal = document.querySelector("#colors")
 let total = document.querySelector("#total")
 
 botonCalcular.addEventListener("click", function(){
 let valorFinal = cantidad.value * precio;
 nombreUsuario.innerHTML = nombre.value;
+cantidadFinal.innerHTML = cantidad.value;
 total.innerHTML = valorFinal;
 colorFinal.style.backgroundColor = color.value;
 
